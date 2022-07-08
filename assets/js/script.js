a653d4348e11631a45ada4a5a6988aaa0e9fcfbd
Dũng Hoàng <hoangtrungdungxz@gmail.com>
2022-07-09 04:31:00 +0700

    client say

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,20 @@ $(document).ready(function () {
 			$('.header-navbar').removeClass('active');
 		}
 	});
+
+	$('.product-tab a').on('click', function (e) {
+		const tab_target = $(this).attr('tab-target');
+		// Xóa active của các product-list và product-tab a
+		$('.product-list').removeClass('active');
+		$('.product-tab a').removeClass('active');
+		// /Khi click vào product-tab nào thì nó sẽ được thêm class active
+		// đồng thời product list có id = giá trị của thuộc tính tab - target cũng được thêm class active
+		$(this).addClass('active');
+		$(tab_target).addClass('active');
+	});
+
+	$('.client-say-slick').slick({
+		dots: true, // hiện dot
+		arrows: false, // bỏ mũi tên 2 bên
+	});
 });
